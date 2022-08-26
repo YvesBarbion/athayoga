a04bbbef9af78d1d8cb09dce1e490bb25039bae2
Yves Barbion <yves.athayoga@gmail.com>
2022-08-26 16:35:28 +0200

    .

--- DITA-topics/out/bea_test_en-us/oxygen-webhelp/template/resources/js/line-number.js
+++
@@ -1,6 +0,0 @@
-﻿var pre = document.querySelectorAll('pre'),
-pl = pre.length;
-for (var i = 0; i < pl; i++) {
-    pre[i].classList.add("line-numbers");
-	pre[i].classList.add("language-css");
-}
--- DITA-topics/out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
+++
@@ -1,4 +0,0 @@
-/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-define(function () {
-return {6:7,7:-1,3:7,0:-1,1:-1,9:7,8:7,5:-1};
-});
